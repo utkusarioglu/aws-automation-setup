971f0e91708d3b1ced72e19fb35b8e963d4a5293
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-04-24 12:46:01 +0300

    Cosmetic changes
    providers.tf
    - `terraform fmt` related changes
    
    src/workshop-aws-terraform/connect-provider.tf
    - Remove unneeded comment

--- src/workshop-aws-terraform/connect-provider.tf
+++ src/workshop-aws-terraform/connect-provider.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # github
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] 
   client_id_list = [
     "sts.amazonaws.com"
   ]
