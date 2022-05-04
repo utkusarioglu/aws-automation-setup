cdc2cb2889cfd3dbbbdab475d8a9208430e7a7d8
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-04 19:49:56 +0300

    Feat: add bucket for utkusarioglu.com static
    src/utkusarioglu-com/s3.tf
    - Add bucket at `us-west-2` for utkusarioglu.com's static content.

--- src/utkusarioglu-com/s3.tf
+++ src/utkusarioglu-com/s3.tf
@@ -1,4 +1,4 @@
-# Stores tf state for the repo `workshop-aws-terraform`
+# State store
 resource "aws_s3_bucket" "state_store" {
   bucket = "utkusarioglu-com-state-store"
   tags   = local.default_tags
@@ -17,5 +17,25 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_store" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+# Static content bucket
+# Note that this one doesn't use the default provider
+resource "aws_s3_bucket" "static_content" {
+  provider = aws.west_2
+  bucket = "utkusarioglu-com-static-content"
+}
+
+resource "aws_s3_bucket_website_configureation" "static_content" {
+  provider = aws.west_2
+  bucket = aws_s3_bucket.static_content.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    suffix = "error.html"
   }
 }
