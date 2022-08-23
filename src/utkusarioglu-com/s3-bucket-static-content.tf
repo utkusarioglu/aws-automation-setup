f953ec328837cec2ff0094b7af591c92479db207
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-23 14:12:54 +0300

    Refactor: Rename file

--- src/utkusarioglu-com/s3-bucket-static-content.tf
+++ src/utkusarioglu-com/s3-bucket-static-content.tf
@@ -29,3 +29,9 @@ resource "aws_s3_bucket_cors_configuration" "static_content" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_policy" "static_content" {
+  provider = aws.us_west_2
+  bucket   = aws_s3_bucket.static_content.bucket
+  policy   = data.aws_iam_policy_document.static_content_public_read.json
+}
