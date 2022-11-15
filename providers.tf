8a2aa0a880411faeff7930ea7485eb576c6832b3
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-15 08:51:33 +0000

    Chore: Subscribe to `tf-repo-template` as template
    Introduces the minimal changes to adapt the repo to subscribe to
    `tf-repo-template` as its template repo.

--- providers.tf
+++ providers.tf
@@ -6,24 +6,43 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "utkusarioglu-aws-automation-setup-state"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "utkusarioglu-aws-automation-setup-lock"
+    bucket                  = "utkusarioglu-aws-automation-setup-state"
+    key                     = "terraform.tfstate"
+    region                  = "eu-central-1"
+    dynamodb_table          = "utkusarioglu-aws-automation-setup-lock"
+    shared_credentials_file = "./.secrets/aws/credentials"
+    profile                 = "aws-automation-setup"
   }
 }
 
 provider "aws" {
-  alias  = "eu_central_1"
-  region = "eu-central-1"
+  region                   = "eu-central-1"
+  shared_config_files      = ["./.secrets/aws/config"]
+  shared_credentials_files = ["./.secrets/aws/credentials"]
+  profile                  = "aws-automation-setup"
+}
+
+
+provider "aws" {
+  alias                    = "eu_central_1"
+  region                   = "eu-central-1"
+  shared_config_files      = ["./.secrets/aws/config"]
+  shared_credentials_files = ["./.secrets/aws/credentials"]
+  profile                  = "aws-automation-setup"
 }
 
 provider "aws" {
-  alias  = "us_west_2"
-  region = "us-west-2"
+  alias                    = "us_west_2"
+  region                   = "us-west-2"
+  shared_config_files      = ["./.secrets/aws/config"]
+  shared_credentials_files = ["./.secrets/aws/credentials"]
+  profile                  = "aws-automation-setup"
 }
 
 provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
+  alias                    = "us_east_1"
+  region                   = "us-east-1"
+  shared_config_files      = ["./.secrets/aws/config"]
+  shared_credentials_files = ["./.secrets/aws/credentials"]
+  profile                  = "aws-automation-setup"
 }
